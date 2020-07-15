b424c9b5b2f944dc69d15d4697c2eb992bb2a7e3
Edoardo Riggio <edo.riggio19@gmail.com>
2020-07-15 18:37:37 +0200

    redone merge sort + plots

--- first_half/sorting/README.md
+++ first_half/sorting/README.md
@@ -8,14 +8,42 @@ Insertion sort is an efficient algorithm when we have a small array to sort. We 
 array, and check if the number is smaller than the number before it. At any time, we will have the left-hand side of
 the array to be sorted. Below is the pseudocode representation of the algorithm.
 
+### Design
+
+Incremental
+
 ### Complexity
 
 - **Best Case** - Θ(n)
 - **Average Case** - Θ(n^2)
 - **Worst Case** - Θ(n^2)
 
-### Execution Times - [[script](/plots/insertion_sort_plot.py)
+### Execution Times - [[script](/plots/insertion_sort_plot.py)]
 
 <p align="center">
-  <img src="/plots/assets/insertion_sort.png" alt="Insertion sort">
+  <img src="/plots/assets/insertion_sort.png" alt="insertion sort">
+</p>
+
+## Merge Sort
+
+### Abstract
+
+The merge sort algorithm takes advantage of the divide-and-conquer-approach. The initial array is divided in half
+recursively. The subarrays are sorted recursively using merge sort. Finally the two subarrays (i.e. the two halves)
+are merged together.
+
+### Design
+
+Divide-and-Conquer
+
+### Complexity
+
+- **Best Case** - Θ(n log(n))
+- **Average Case** - Θ(n log(n))
+- **Worst Case** - Θ(n log(n))
+
+### Execution Times - [[script](/plots/merge_sort_plot.py)]
+
+<p align="center">
+  <img src="/plots/assets/merge_sort.png" alt="merge sort">
 </p>
--- first_half/sorting/merge_sort.py
+++ first_half/sorting/merge_sort.py
@@ -12,28 +12,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-numbs1 = [int(i) for i in input().split(' ')]
-numbs2 = [int(i) for i in input().split(' ')]
+# Time complexities:
+# Best case -> O(n log(n))
+# Average case -> O(n log(n))
+# Worst case -> O(n log(n))
+
+from math import ceil
+
 
 def merge(numbs1: list, numbs2: list) -> list:
     """
     Go through the two sorted arrays simultaneously from
-    left to right. Find the smallest element between the 
-    two in the two arrays. Append the smallest element 
+    left to right. Find the smallest element between the
+    two in the two arrays. Append the smallest element
     to a third array and increase the index from which the
     element was taken by one. If the two elements are the
     same, then add one of them to the third array and
-    increase both indexes by one. The process is repeated 
+    increase both indexes by one. The process is repeated
     until the end of both arrays. If one of the two arrays
     is bigger, then add the remaining elements of the biggest
     array at the end of the iterations.
 
-    Args:
-        numbs1 (list): The first array to be merged
-        numbs2 (list): The second array to be merged
-
-    Returns:
-        (list) The sorted array
+    :param numbs1: The first array to be merged
+    :param numbs2: The second array to be merged
+    :return: The sorted array
     """
     sorted_numbs = []
     i = j = 0
@@ -52,18 +54,35 @@ def merge(numbs1: list, numbs2: list) -> list:
 
     if i < len(numbs1):
         sorted_numbs += numbs1[i:]
-    
+
     if j < len(numbs2):
         sorted_numbs += numbs2[j:]
 
     return sorted_numbs
 
-# Test with user input
-print(merge(numbs1, numbs2))
 
-# Tests without user input
-assert merge([1,3,5,7,9], [2,4,6,8,10]) == [1,2,3,4,5,6,7,8,9,10]
-assert merge([2,5,7], [1,6,7,13,15]) == [1,2,5,6,7,13,15]
-assert merge([1,2,6,8,9,12], [1,5,7,8]) == [1,2,5,6,7,8,9,12]
-assert merge([1,2,6,8,9,12], []) == [1,2,6,8,9,12]
-assert merge([], [1,5,7,8]) == [1,5,7,8]
+def merge_sort(array: list) -> list:
+    """
+    Find the midpoint of the array and divide it into
+    two subarray. This is repeated until the subarrays
+    have all length = 1. After the array has been
+    divided, apply the merge algorithm on the subarrays
+    in order to obtain a second_half sorted array (without
+    duplicates)
+
+    :param array: The array to be sorted
+    :return: The sorted array
+    """
+    if len(array) < 2:
+        return array
+
+    mid = ceil(len(array) / 2)
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
+
+    return merge(left, right)
+
+
+if __name__ == '__main__':
+    data = [int(i) for i in input().split(" ")]
+    print(merge_sort(data))
